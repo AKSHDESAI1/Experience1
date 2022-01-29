29b99f1fa553ffce11ed545d1af05df2ade6601a
meshv-p <77038149+meshv-p@users.noreply.github.com>
2022-01-29 21:54:43 +0530

    Upload done with name

--- expapp/models.py
+++ expapp/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class uploadDocument(models.Model):
     files= models.FileField(upload_to='files')
     date_time = models.DateField(auto_now=True)
+    name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
--- expapp/views.py
+++ expapp/views.py
@@ -5,7 +5,7 @@ from expapp.models import uploadDocument
 # Create your views here.
 def upload(request):
     if request.method == 'POST':
-        print(request.FILES['file'])
-        doc = uploadDocument(files=request.FILES['file'])
+        print(type(request.FILES['file']))
+        doc = uploadDocument(name = request.FILES['file'],files=request.FILES['file'])
         doc.save()
     return render(request,'upload.html')

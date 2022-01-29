490724590afc2ec4201f544768d2ed06667868a9
AKSHDESAI1 <desaiaksh82@gmail.com>
2022-01-29 19:12:14 +0530

    Login Google Done

--- exppro/urls.py
+++ exppro/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('accounts/', include('allauth.urls')),
     path('', TemplateView.as_view(template_name='base.html'), name='home'),
     path('login1/', views.login1, name='login1'),
     path('signup1/', views.signup1, name='signup1'),
